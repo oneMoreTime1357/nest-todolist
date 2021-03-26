5f9e216b300190c71b780c5ecc9d7b198e3ea771
dan yang <yangdan1357@gmail.com>
2021-03-26 20:16:23 +0800

    add module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Get, Req, Post, Param } from '@nestjs/common';
+import { Controller, Get, Req, Post, Param, Body } from '@nestjs/common';
+import { Request } from 'express'
+import { ResponseData } from '../common/interfaces/result.interface'
+import { CreateUserDto } from './dto/create-user.dto'
 
 @Controller('users')
 export class UsersController {
   @Get()
-  findAll(@Req() request: Request): string {
+  findAll(@Req() request: Request): Promise<ResponseData> {
     return 'This action returns all cats';
   }
 
   @Post()
-  create(): string {
+  create(@Body() createUserDto: CreateUserDto): string {
     return 'This action adds a new cat';
   }
 
